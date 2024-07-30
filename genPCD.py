16828b7f1e03b735edc5b4bd32f134baec291eb2
Akshaj <akshaj.kanumuri@gmail.com>
2024-07-30 00:43:24 -0700

    finished full pipeline

--- genPCD.py
+++ genPCD.py
@@ -2,17 +2,36 @@ import open3d as o3d
 import numpy as np
 import os
 
-color_raw = o3d.io.read_image('outputs/color/color_IMG_1421.png')
-depth_raw = o3d.io.read_image('outputs/depth/depth_IMG_1421.png')
+color_raw = []
+depth_raw = []
 
-rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw)
+camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(
+    o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
 
-camera_intrinsic = o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+for filename in os.listdir('outputs/color'):
+    filepath = os.path.join('outputs/color', filename)
+    color_raw.append(filepath)
 
-pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
+for filename in os.listdir('outputs/depth'):
+    filepath = os.path.join('outputs/depth', filename)
+    depth_raw.append(filepath)
 
-# mirror the image so it does not appear upside down
-pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+final_pcd = o3d.geometry.PointCloud()
 
-o3d.visualization.draw_geometries([pcd])
-o3d.io.write_point_cloud('test.ply', pcd, format='auto', write_ascii=False)
+for color_path, depth_path in zip(color_raw, depth_raw):
+    color = o3d.io.read_image(color_path)
+    depth = o3d.io.read_image(depth_path)
+
+    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color, depth)
+
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, camera_intrinsic)
+
+    pcd.transform([[1, 0, 0, 0],
+                   [0, -1, 0, 0],
+                   [0, 0, -1, 0],
+                   [0, 0, 0, 1]])
+    
+    final_pcd += pcd
+
+o3d.visualization.draw_geometries([final_pcd])
+o3d.io.write_point_cloud('final.ply', final_pcd, format='auto', write_ascii=False)
